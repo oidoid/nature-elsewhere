f14a8979b5cb14fa9232d2c9b100227b9372360c
Stephen Niedzielski <stephen@niedzielski.com>
2017-12-12 18:54:44 -0600

    New: add a pond sprite
    If texture scaling is needed later for whatever reason, the following
    works:
    
      Texture.minFilter = Three.NearestFilter;
      Texture.magFilter = Three.NearestFilter;
    
    Via: https://github.com/mrdoob/three.js/issues/1418#issuecomment-4219747

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,48 @@
 import * as Three from "three";
 
+// An integer for pixel alignment.
 const HEIGHT = 192;
 
+// Consistently call Math.round() on the result of width() to keep pixel
+// alignment.
 function width() {
   const ratio = window.innerWidth / window.innerHeight;
   return HEIGHT * ratio;
 }
 
 const renderer = new Three.WebGLRenderer();
-// No smoothing. Pixels are one for one when rendering.
+// No doubling. Pixels are one for one when rendering.
 renderer.setPixelRatio(1);
 document.body.appendChild(renderer.domElement);
 
-const camera = new Three.OrthographicCamera(0, width(), 0, HEIGHT);
+const camera = new Three.OrthographicCamera(0, Math.round(width()), 0, HEIGHT);
 
-const geometry = new Three.BoxGeometry(10, 10, 10);
+const scene = new Three.Scene();
+
+const texture = new Three.TextureLoader().load("assets/images/pond1.png");
+texture.flipY = false;
+const spriteMaterial = new Three.SpriteMaterial({ map: texture });
+const sprite = new Three.Sprite(spriteMaterial);
+sprite.position.set(0, 0, -1);
+sprite.scale.set(128, 16, 1);
+scene.add(sprite);
+
+const geometry = new Three.BoxGeometry(10, 10, 0);
 const material = new Three.MeshBasicMaterial({ color: 0x00ff00 });
 const cube = new Three.Mesh(geometry, material);
-const scene = new Three.Scene();
 scene.add(cube);
 
-function render() {
-  requestAnimationFrame(render);
-
+function render(clock: Three.Clock) {
+  // console.log(clock.getDelta());
   cube.rotation.z += 0.033;
 
   renderer.render(scene, camera);
+
+  requestAnimationFrame(() => render(clock));
 }
 
 resize();
-render();
+render(new Three.Clock());
 
 window.addEventListener("resize", resize);
 
@@ -38,15 +51,14 @@ function resize() {
   console.log(
     `resize: ` +
       `window=${window.innerWidth}x${window.innerHeight} ` +
-      `canvas=${width()}x${HEIGHT} ` +
+      `canvas=${Math.round(width())}x${HEIGHT} ` +
       `ratio=${window.innerWidth / window.innerHeight}`
   );
-  renderer.setSize(width(), HEIGHT);
+  renderer.setSize(Math.round(width()), HEIGHT);
 
-  camera.right = width();
-  camera.bottom = HEIGHT;
-  camera.position.x = -width() / 2;
-  camera.position.y = -HEIGHT / 2;
+  camera.right = Math.round(width());
+  camera.position.x = Math.round(-width() / 2);
+  camera.position.y = Math.round(-HEIGHT / 2);
 
   camera.updateProjectionMatrix();
 }
