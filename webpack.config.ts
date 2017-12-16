788ca555a5725a152bf375821681bc821342b821
Stephen Niedzielski <stephen@niedzielski.com>
2017-12-16 15:20:00 -0600

    Chore: show Webpack warnings and errors
    Silence is golden. Add all the boilerplate necessary to be very loud
    about warnings and errors, and very quiet about everything else.

--- webpack.config.ts
+++ webpack.config.ts
@@ -3,18 +3,39 @@ import * as CleanPlugin from 'clean-webpack-plugin'
 import * as CopyWebpackPlugin from 'copy-webpack-plugin'
 import * as webpack from 'webpack'
 
+const PRODUCTION = process.env.NODE_ENV === 'production'
+
+const STATS = {
+  all: false,
+  colors: true,
+  errors: true,
+  errorDetails: true,
+  moduleTrace: true,
+  warnings: true
+}
+
 const config: webpack.Configuration = {
   entry: './src',
   resolve: {extensions: ['.ts', '.js']},
 
   output: {path: path.resolve('build'), filename: 'index.js'},
 
-  module: {
-    rules: [{test: /\.ts$/, use: 'ts-loader'}]
-  },
+  module: {rules: [{test: /\.ts$/, use: 'ts-loader'}]},
+
+  stats: STATS,
+
+  devServer: PRODUCTION
+    ? undefined
+    : {
+        clientLogLevel: 'warning',
+        progress: false,
+        noInfo: true,
+        overlay: {warnings: true, errors: true},
+        stats: STATS
+      },
 
   plugins: [
-    new CleanPlugin('build'),
+    new CleanPlugin('build', {verbose: false}),
     new CopyWebpackPlugin([
       {from: 'src/index.html'},
       {from: 'src/assets', to: 'assets'}
