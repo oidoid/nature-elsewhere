47bf87c67d8c2180a9b61c4a722689b2bcdb689d
Stephen Niedzielski <stephen@niedzielski.com>
2018-03-11 10:42:27 -0500

    Update: check all GL operations

--- src/gfx/gl.ts
+++ src/gfx/gl.ts
@@ -7,75 +7,41 @@ export type GLUniformLocation = WebGLUniformLocation
 export function check(gl: GL | null): GL {
   if (!gl) throw new Error('WebGL context unobtainable.')
 
+  type Indexed = {[prop: string]: any}
   const proto: GL = Object.getPrototypeOf(gl)
-  return Object.assign(gl, {
-    compileShader(shader: GLShader | null) {
-      proto.compileShader.apply(gl, arguments)
-      if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
-        const log = gl.getShaderInfoLog(shader)
-        const msg = `Shader compilation failed; error=${gl.getError()}:\n${log}`
-        throw new Error(msg)
-      }
-    },
-    createBuffer() {
-      const buffer = proto.createBuffer.apply(gl, arguments)
-      if (!buffer) {
-        throw new Error(`Buffer creation failed; error=${gl.getError()}.`)
-      }
-      return buffer
-    },
-    createProgram() {
-      const program = proto.createProgram.apply(gl, arguments)
-      if (!program) {
-        const msg = `Shader program creation failed; error=${gl.getError()}.`
-        throw new Error(msg)
-      }
-      return program
-    },
-    createShader() {
-      const shader = proto.createShader.apply(gl, arguments)
-      if (!shader) {
-        throw new Error(`Shader creation failed; error=${gl.getError()}.`)
-      }
-      return shader
-    },
-    createTexture() {
-      const texture = proto.createTexture.apply(gl, arguments)
-      if (!texture) {
-        throw new Error(`Texture creation failed; error=${gl.getError()}.`)
-      }
-      return texture
-    },
-    getAttribLocation(_program: GLProgram, name: string) {
-      const location = proto.getAttribLocation.apply(gl, arguments)
-      if (location < 0) {
-        const msg =
-          'Shader attribute location unknown; ' +
-          `name=${name} location=${location} error=${gl.getError()}.`
-        throw new Error(msg)
-      }
-      return location
-    },
-    getUniformLocation(_program: GLProgram, name: string) {
-      const location = proto.getUniformLocation.apply(gl, arguments)
-      if (location === null || location < 0) {
-        const msg =
-          'Shader uniform location unknown; ' +
-          `name=${name} location=${location} error=${gl.getError()}.`
-        throw new Error(msg)
-      }
-      return location
-    },
-    linkProgram(program: GLProgram | null) {
-      proto.linkProgram.apply(gl, arguments)
-      gl.validateProgram(program)
+  return Object.assign(
+    gl,
+    ...Object.keys(proto)
+      .filter(prop => typeof (<Indexed>gl)[prop] === 'function')
+      .map(prop => ({
+        [prop]: function() {
+          const ret = (<Indexed>proto)[prop].apply(gl, arguments)
+          const err = gl.getError()
+          if (err !== gl.NO_ERROR) {
+            const invocation = `${prop}(${Array.from(arguments)}) => ${ret}`
+            throw new Error(`${invocation}; getError() => ${err}`)
+          }
+          return ret
+        }
+      })),
+    {
+      getError: proto.getError,
+      compileShader(shader: GLShader | null) {
+        proto.compileShader.apply(gl, arguments)
+        if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+          const log = gl.getShaderInfoLog(shader)
+          throw new Error(`Shader compilation failed: ${log}`)
+        }
+      },
+      linkProgram(program: GLProgram | null) {
+        proto.linkProgram.apply(gl, arguments)
+        gl.validateProgram(program)
 
-      if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
-        const error = gl.getError()
-        const log = gl.getProgramInfoLog(program)
-        const msg = `Shader program linking failed; error=${error}:\n${log}`
-        throw new Error(msg)
+        if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
+          const log = gl.getProgramInfoLog(program)
+          throw new Error(`Shader linking failed: ${log}`)
+        }
       }
     }
-  })
+  )
 }
