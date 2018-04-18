a639ec6b24010627fcb04b305e582b309a7e6a9c
Stephen Niedzielski <stephen@niedzielski.com>
2018-04-17 21:14:22 -0500

    New: add Aseprite file format

---
+++ src/assets/textures/aseprite.ts
@@ -0,0 +1,151 @@
+/**
+ * This typing assumes the options specified in package.json and annotated
+ * herein with *via CLI*. The JSON export format appears to not be documented
+ * but the related [binary format] is. Types marked "*by convention*" are
+ * supplemental to and unenforced by the JSON format. Any data of these types
+ * should be validated as soon as possible. All numbers are integers. All
+ * indices are zero-based. All geometry are described from the top left to the
+ * bottom right in pixel units:
+ *
+ *       y
+ *       |0 1 2 …
+ *   x---+------►
+ *     0 |
+ *     1 |
+ *     2 |
+ *     : ▼
+ *
+ * [binary format]: https://github.com/aseprite/aseprite/blob/master/docs/ase-file-specs.md.
+ */
+
+/**
+ * The topmost data type for JSON exported from Aseprite. This format contains
+ * all the image, animation, and collision information for every file packed in
+ * the atlas.
+ */
+export type AsepriteFile = {
+  meta: Meta
+  /**
+   * All Frames for all files packed. name is a Tag followed by a space followed
+   * by an optional frame number *via CLI*
+   * `--filename-format '{tag} {frame}' --format json-hash`. E.g., 'cloud xl 4'
+   * refers to the file named "cloud.aseprite" with animation named "xs", frame
+   * index 4, and 'sky  ' refers to the file named "sky.aseprite" with animation
+   * named "", the first frame.
+   */
+  frames: {[name: string]: Frame}
+}
+
+export type Meta = {
+  /** E.g., 'http://www.aseprite.org/'. */
+  app: string
+  /** E.g., '1.2.8.1'. */
+  version: string
+  /** The associated output. E.g., 'atlas.png'. */
+  image: string
+  /** E.g., 'RGBA8888'. */
+  format: string
+  /** Output dimensions. *Via CLI* `--sheet-pack`, uses a power of 2. */
+  size: WH
+  /** E.g., '1'. */
+  scale: string
+  /** All FrameTags for all files packed *via CLI* `--list-tags`. */
+  frameTags: FrameTag[]
+  /** All slices for all files packed *via CLI* `--list-slices`. */
+  slices: Slice[]
+}
+
+/** A single animation frame. Each file packed always has at least one Frame. */
+export type Frame = {
+  /**
+   * The Frame's bounds within the atlas, including a 1px border padding
+   * *via CLI* `--inner-padding 1`. The padding dimensions may also be
+   * calculated by subtracting member's WH dimensions from sourceSize and
+   * dividing by 2.
+   */
+  frame: XY & WH
+  rotated: boolean
+  trimmed: boolean
+  /** The Frame's bounds within the file packed, not including padding. */
+  spriteSourceSize: XY & WH
+  sourceSize: WH
+  duration: Duration
+}
+
+/** A label and animation behavior for one or more Frames. */
+export type FrameTag = {
+  name: Tag
+  /** The inclusive starting Frame index. */
+  from: number
+  /**
+   * The inclusive ending Frame index, possibly identical to the starting frame
+   * index.
+   */
+  to: number
+  direction: Direction
+}
+
+/**
+ * *By convention*, tags are a file stem followed by a space followed by a
+ * possibly empty animation name. E.g., 'cactus xs' describes the file named
+ * "cactus.aseprite" with animation named "xs" and 'sun ' refers to the file
+ * named "sun.aseprite" with animation named "".
+ */
+export type Tag = string
+
+/** Animation length in milliseconds. */
+export type Duration = number
+
+/**
+ * *By convention*, animations that should never end have this reserved value.
+ */
+export const INFINITE_DURATION: Duration = 65535
+
+/** An animation's looping behavior. */
+export type Direction =
+  /** Animate from start to end; when looping, return to start. */
+  | 'forward'
+  /** Animate from end to start; when looping, return to end. */
+  | 'reverse'
+  /**
+   * Animate from start to end - 1 or start, whichever is greater; when looping,
+   * change direction (initially, end to start + 1 or end, whichever is lesser.
+   * Traversals from start to end - 1 and end to start + 1 are each considered
+   * complete loops.
+   */
+  | 'pingpong'
+
+/**
+ * *By convention*, a collection of bounds within the file packed whose union
+ * defines the total collision polygon for a single Frame.
+ */
+export type Slice = {
+  name: Tag
+  /** Color in #rrggbbaa format. E.g., blue is '#0000ffff'. */
+  color: string
+  keys: Key[]
+}
+
+/** A Frame collision boundary subset within the file packed. */
+export type Key = {
+  /** The associated Frame's index. */
+  frame: number
+  /** The Frame's collision boundary within the file packed. */
+  bounds: XY & WH
+}
+
+/** Width and height lengths. */
+export type WH = {
+  /** Width. */
+  w: number
+  /** Height. */
+  h: number
+}
+
+/** x and y-coordinates. */
+export type XY = {
+  /** Distance along the x-axis. */
+  x: number
+  /** Distance along the y-axis. */
+  y: number
+}
