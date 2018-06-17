6151a6e14f59f71bf8bfad062b1d1ece927040b8
Stephen Niedzielski <stephen@niedzielski.com>
2018-06-16 22:18:36 -0500

    Update: extend the grass

--- src/assets/levels/level0.ts
+++ src/assets/levels/level0.ts
@@ -1,10 +1,19 @@
 import * as palette from './palette'
 import * as SpriteFactory from '../sprites/sprite-factory'
+import {Sprite} from '../sprites/sprite'
 
 export const PLAYER = SpriteFactory.newPlayer({x: 35, y: 60})[0]
-export const GRASS_L = SpriteFactory.newGrassL({x: 16, y: 60})
-  .concat(SpriteFactory.newGrassL({x: 160, y: 60}))
-  .concat(SpriteFactory.newGrassL({x: 176, y: 60}))
+
+export const GRASS_L: Sprite[] = []
+const minRenderHeight = 128
+const width = 16
+for (let x = -minRenderHeight * 8; x <= 16; x += width) {
+  GRASS_L.push(...SpriteFactory.newGrassL({x, y: 60}))
+}
+
+GRASS_L.concat(SpriteFactory.newGrassL({x: 160, y: 60})).concat(
+  SpriteFactory.newGrassL({x: 176, y: 60})
+)
 export const TREE = SpriteFactory.newTree({x: 185, y: 44})
 export const CLOUDS = SpriteFactory.newCloudS({x: 40, y: 20})
   .concat(SpriteFactory.newCloudM({x: 58, y: 16}))
