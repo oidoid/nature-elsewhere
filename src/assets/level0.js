b179edf0e95ab7bfec223697516b3ae8cfd46f5f
Stephen Niedzielski <stephen@niedzielski.com>
2018-11-02 17:22:41 -0600

    Chore: allow drawables and animatables in store

--- src/assets/level0.js
+++ src/assets/level0.js
@@ -9,13 +9,12 @@ import * as superBall from '../entities/super-ball.js'
 import * as tallGrassPatch from '../entities/tall-grass-patch.js'
 import * as util from '../util.js'
 import {AnimationID} from './animation-id.js'
-import {EntityID} from '../entities/entity-id.js'
 import {Limits} from '../graphics/limits.js'
 
 /**
  * @typedef {Readonly<{
  *   player: entity.State
- *   entities: ReadonlyArray<entity.State>
+ *   entities: ReadonlyArray<entity.State|animatable.State|drawable.State>
  * }>} Level0
  */
 
@@ -25,45 +24,24 @@ import {Limits} from '../graphics/limits.js'
  * @return {Level0}
  */
 export function newState(atlas, randomState) {
-  const animatables = [
-    animatable.newState(
-      drawable.newState(
-        AnimationID.PALETTE_PALE,
-        {x: Limits.HALF_MIN, y: Limits.HALF_MIN},
-        {x: Limits.MAX, y: Limits.MAX}
-      )
-    ),
-    animatable.newState(
-      drawable.newState(AnimationID.GRASS_L, {x: -512, y: -12}, {x: 480, y: 1})
-    ),
-    animatable.newState(
-      drawable.newState(AnimationID.GRASS_L, {x: 208, y: -12}, {x: 2, y: 1})
-    ),
-    animatable.newState(drawable.newState(AnimationID.HILL, {x: 40, y: -28})),
-    animatable.newState(
-      drawable.newState(AnimationID.GRASS_L, {x: 228, y: -12}, {x: 6, y: 1})
-    ),
-    animatable.newState(drawable.newState(AnimationID.TREE, {x: 185, y: -39}))
-  ]
   const entities = [
-    tallGrassPatch.newState({x: 188, y: -15}, 1000, randomState),
-    entity.newState(EntityID.BACKGROUND, animatables),
-    entity.newState(
-      EntityID.CLOUD,
-      [animatable.newState(drawable.newState(AnimationID.CLOUD_S))],
-      {x: 40, y: -60}
+    drawable.newState(
+      AnimationID.PALETTE_PALE,
+      {x: Limits.HALF_MIN, y: Limits.HALF_MIN},
+      {x: Limits.MAX, y: Limits.MAX}
     ),
-    entity.newState(
-      EntityID.CLOUD,
-      [animatable.newState(drawable.newState(AnimationID.CLOUD_M))],
-      {x: 58, y: -76},
+    drawable.newState(AnimationID.GRASS_L, {x: -512, y: -12}, {x: 480, y: 1}),
+    drawable.newState(AnimationID.GRASS_L, {x: 208, y: -12}, {x: 2, y: 1}),
+    drawable.newState(AnimationID.HILL, {x: 40, y: -28}),
+    drawable.newState(AnimationID.GRASS_L, {x: 228, y: -12}, {x: 6, y: 1}),
+    drawable.newState(AnimationID.TREE, {x: 185, y: -39}),
+    tallGrassPatch.newState({x: 188, y: -15}, 1000, randomState),
+    drawable.newState(AnimationID.CLOUD_S, {x: 40, y: -60}),
+    animatable.newState(
+      drawable.newState(AnimationID.CLOUD_M, {x: 58, y: -76}),
       {x: -0.0005, y: 0}
     ),
-    entity.newState(
-      EntityID.CLOUD,
-      [animatable.newState(drawable.newState(AnimationID.CLOUD_XL))],
-      {x: 120, y: -60}
-    ),
+    drawable.newState(AnimationID.CLOUD_XL, {x: 120, y: -60}),
     rainCloud.newState(AnimationID.CLOUD_S, {x: 75, y: -65}, -0.0001),
     rainCloud.newState(AnimationID.CLOUD_L, {x: 20, y: -81}, -0.00008),
     ...util.range(0, 1000).map(i => {
--- src/store.js
+++ src/store.js
@@ -1,15 +1,19 @@
+import * as animatable from './drawables/animatable.js'
 import * as entity from './entities/entity.js'
 import * as recorder from './inputs/recorder.js'
 import * as shader from './graphics/shader.js'
+import {AnimationLayer} from './assets/animation-layer.js'
 import {Behavior} from './entities/behavior.js'
 import {EntityLayer} from './entities/entity-layer.js'
+import {Layer} from './drawables/layer.js'
 
-/** @typedef {import('./drawables/atlas.js').Atlas} Atlas} */
+/** @typedef {import('./drawables/atlas').Atlas} Atlas} */
+/** @typedef {import('./drawables/drawable').State} Drawable} */
 
 export class Store {
   constructor() {
     /** @prop {Int16Array} */ this._memory = new Int16Array()
-    /** @prop {ReadonlyArray<entity.State>} */ this._entities = /** @type {entity.State[]} */ ([])
+    /** @prop {ReadonlyArray<entity.State|animatable.State|Drawable>} */ this._entities = /** @type {(entity.State|animatable.State|Drawable)[]} */ ([])
   }
 
   /** @return {Int16Array} */
@@ -19,18 +23,19 @@ export class Store {
 
   /** @return {number} */
   getLength() {
-    return this._entities.reduce((sum, val) => sum + val.animatables.length, 0)
+    return this._entities.reduce(
+      (sum, val) => sum + (isEntity(val) ? val.animatables.length : 1),
+      0
+    )
   }
 
   /**
-   * @arg {ReadonlyArray<entity.State>} entities
+   * @arg {ReadonlyArray<entity.State|animatable.State|Drawable>} entities
    * @return {void}
    */
   spawn(entities) {
     entities.forEach(lhs => {
-      let index = this._entities.findIndex(
-        rhs => EntityLayer[lhs.id] <= EntityLayer[rhs.id]
-      )
+      let index = this._entities.findIndex(rhs => layer(lhs) <= layer(rhs))
       this._entities.splice(
         index === -1 ? this._entities.length : index,
         0,
@@ -47,8 +52,12 @@ export class Store {
    */
   step(stepState, atlas, recorder) {
     this._entities.forEach(val => {
-      const step = Behavior[val.id] || entity.step
-      step(val, stepState, atlas, recorder)
+      if (isEntity(val)) {
+        const step = Behavior[val.id] || entity.step
+        step(val, stepState, atlas, recorder)
+      } else if (isAnimatable(val)) {
+        animatable.step(val, stepState, atlas.animations[val.animationID])
+      }
     })
   }
 
@@ -61,22 +70,74 @@ export class Store {
     if (this._memory.length < minMemory) {
       this._memory = new Int16Array(minMemory * 2)
     }
-    let index = 0
 
+    let index = 0
     this._entities.forEach(val => {
-      val.animatables.forEach((_, i) => {
-        const coord = entity.coord(val, i, atlas)
-        const scrollPosition = entity.scrollPosition(val, i)
-        const position = entity.position(val, i)
-        const scale = entity.scale(val, i)
-        // prettier-ignore
-        this._memory.set([coord.x, coord.y, coord.w, coord.h,
-                        scrollPosition.x, scrollPosition.y,
-                        position.x, position.y,
-                        scale.x, scale.y],
-                        index * shader.layout.perInstance.length)
+      if (isEntity(val)) {
+        val.animatables.forEach((_, i) => {
+          const coord = entity.coord(val, i, atlas)
+          const scrollPosition = entity.scrollPosition(val, i)
+          const position = entity.position(val, i)
+          const scale = entity.scale(val, i)
+          flush(this, index, coord, scrollPosition, position, scale)
+          ++index
+        })
+      } else {
+        const animation = atlas.animations[val.animationID]
+        flush(
+          this,
+          index,
+          isAnimatable(val)
+            ? animatable.bounds(val, animation)
+            : animation.cels[0].bounds,
+          val.scrollPosition,
+          val.position,
+          val.scale
+        )
         ++index
-      })
+      }
     })
   }
+}
+
+/**
+ * @arg {Store} state
+ * @arg {number} index
+ * @arg {Rect} coord
+ * @arg {XY} scrollPosition
+ * @arg {XY} position
+ * @arg {XY} scale
+ * @return {void}
+ */
+function flush(state, index, coord, scrollPosition, position, scale) {
+  // prettier-ignore
+  state._memory.set([coord.x, coord.y, coord.w, coord.h,
+                     scrollPosition.x, scrollPosition.y,
+                     position.x, position.y,
+                     scale.x, scale.y],
+                     index * shader.layout.perInstance.length)
+}
+
+/**
+ * @arg {entity.State|Drawable} val
+ * @return {Layer}
+ */
+function layer(val) {
+  return isEntity(val) ? EntityLayer[val.id] : AnimationLayer[val.animationID]
+}
+
+/**
+ * @arg {entity.State|animatable.State|Drawable} val
+ * @return {val is entity.State}
+ */
+function isEntity(val) {
+  return 'animatables' in val
+}
+
+/**
+ * @arg {entity.State|animatable.State|Drawable} val
+ * @return {val is animatable.State}
+ */
+function isAnimatable(val) {
+  return 'animator' in val
 }
