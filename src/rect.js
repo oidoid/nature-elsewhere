ec849fb28110082d43bf97ddef2ac2577e0477fd
Stephen Niedzielski <stephen@niedzielski.com>
2018-12-02 09:44:20 -0700

    Update: add rectangle intersection tests

---
+++ src/rect.js
@@ -0,0 +1,36 @@
+/**
+ * @arg {Rect} lhs
+ * @arg {Rect} rhs
+ * @return {boolean} True if lhs and rhs are touching or overlapping, false if
+ *                   independent.
+ */
+export function intersects(lhs, rhs) {
+  const overlap = intersection(lhs, rhs)
+  return overlap.w >= 0 && overlap.h >= 0
+}
+
+/**
+ * @arg {Rect} lhs
+ * @arg {Rect} rhs
+ * @return {Rect} Area is less than zero if no intersection, equal to zero if
+ *                touching but not overlapping, or greater than zero if
+ *                overlapping.
+ */
+export function intersection(lhs, rhs) {
+  const upperLeft = maximumXY(lhs, rhs)
+  return {
+    x: upperLeft.x,
+    y: upperLeft.y,
+    w: Math.min(lhs.x + lhs.w, rhs.x + rhs.w) - upperLeft.x,
+    h: Math.min(lhs.y + lhs.h, rhs.y + rhs.h) - upperLeft.y
+  }
+}
+
+/**
+ * @arg {XY} lhs
+ * @arg {XY} rhs
+ * @return {XY} The bottom-rightmost coordinates.
+ */
+export function maximumXY(lhs, rhs) {
+  return {x: Math.max(lhs.x, rhs.x), y: Math.max(lhs.y, rhs.y)}
+}
--- src/types/geometry.d.ts
+++ src/types/geometry.d.ts
@@ -1,3 +1,8 @@
 type XY = Readonly<{x: number; y: number}>
 type WH = Readonly<{w: number; h: number}>
+
+/**
+ * Where XY describes the upper-left corner, or minimum, and XY + WH the
+ * bottom-right, or maximum.
+ */
 type Rect = XY & WH
