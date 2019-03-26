f0ff4c71486ac83783571052e64b22ada6e72153
Stephen Niedzielski <stephen@niedzielski.com>
2019-03-25 20:05:14 -0600

    Chore: isolate virtual joystick presentation

--- src/entities/ui/virtual-joystick.ts
+++ src/entities/ui/virtual-joystick.ts
@@ -13,12 +13,47 @@ import {XY} from '../../math/xy'
 export class VirtualJoystick {
   private static _maxIdleVisibility: number = 3072
 
+  /** Duration in milliseconds since last stick update. */
+  private _timer: number = 0
+  private _image: VirtualJoystickImage
+
+  constructor(atlas: Atlas.Definition, layer: number) {
+    this._image = new VirtualJoystickImage(atlas, layer)
+  }
+
+  update(milliseconds: number, recorder: Recorder): void {
+    this._timer += milliseconds
+
+    const [set] = recorder.combo().slice(-1)
+
+    if (recorder.triggeredSet(InputBit.POSITION_VIRTUAL_JOYSTICK)) {
+      const position = set[InputSource.VIRTUAL_GAMEPAD_JOYSTICK_POSITION]
+      const xy = (<VirtualJoystickPositionInput>position).xy
+      this._image.setPosition(xy)
+      this._timer = 0
+    }
+
+    const joystick = set && set[InputSource.VIRTUAL_GAMEPAD_JOYSTICK_AXES]
+    if (joystick) {
+      this._image.setStick(joystick.normal, joystick.magnitude)
+      this._timer = 0
+    } else this._image.setStick({x: 0, y: 0}, 0)
+  }
+
+  images(): ReadonlyArray<Image> {
+    return this.hidden() ? [] : this._image.images()
+  }
+
+  private hidden() {
+    return this._timer > VirtualJoystick._maxIdleVisibility
+  }
+}
+
+class VirtualJoystickImage {
   private readonly _base: Image
   private readonly _stick: Image
   private readonly _images: ImageGroup
   private readonly _radiusPx: number
-  /** Duration in milliseconds since last stick update. */
-  private _timer: number = 0
 
   constructor(atlas: Atlas.Definition, layer: number) {
     const palette = Palette.GREYS
@@ -34,42 +69,18 @@ export class VirtualJoystick {
     this._radiusPx = Math.min(this._base.target().w, this._base.target().h) / 2
   }
 
-  update(milliseconds: number, recorder: Recorder): void {
-    this._timer += milliseconds
-
-    const [set] = recorder.combo().slice(-1)
-
-    if (recorder.triggeredSet(InputBit.POSITION_VIRTUAL_JOYSTICK)) {
-      const position = set[InputSource.VIRTUAL_GAMEPAD_JOYSTICK_POSITION]
-      const xy = (<VirtualJoystickPositionInput>position).xy
-      this.setPosition(xy)
-      this._timer = 0
-    }
-
-    const joystick = set && set[InputSource.VIRTUAL_GAMEPAD_JOYSTICK_AXES]
-    if (joystick) {
-      this.setStick(joystick.normal, joystick.magnitude)
-      this._timer = 0
-    } else this.setStick({x: 0, y: 0}, 0)
-  }
-
   images(): ReadonlyArray<Image> {
-    return this.hidden() ? [] : this._images.images()
+    return this._images.images()
   }
 
-  private setPosition(position: XY): void {
-    this._timer = 0
+  setPosition(position: XY): void {
     this._images.moveTo(XY.add(position, -this._radiusPx))
   }
 
-  private setStick(normal: XY, magnitude: number): void {
+  setStick(normal: XY, magnitude: number): void {
     // This is the maximum length of freedom the stick is given.
     const length = Math.min(this._radiusPx, magnitude)
     const target = XY.trunc(XY.mul(normal, length))
     this._stick.moveTo(XY.add(target, this._base.target()))
-  }
-
-  private hidden() {
-    return this._timer > VirtualJoystick._maxIdleVisibility
   }
 }
