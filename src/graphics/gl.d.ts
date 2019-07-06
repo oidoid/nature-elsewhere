a1ca4e53cb914f7e91b195ecdddd9abb925b9da8
Stephen Niedzielski <stephen@niedzielski.com>
2019-07-06 16:39:09 -0600

    [dev] favor interfaces for GL types

--- src/graphics/gl.d.ts
+++ src/graphics/gl.d.ts
@@ -1,8 +1,9 @@
-type GL = WebGL2RenderingContext
-type GLProgram = WebGLProgram
-type GLShader = WebGLShader
-type GLUniformLocation = WebGLUniformLocation | null
+interface GL extends WebGL2RenderingContext {}
 type GLBuffer = WebGLBuffer | null
 type GLBufferData = Parameters<GL['bufferData']>[1]
+type GLContextAttributes = WebGLContextAttributes | undefined
 type GLLoseContext = WEBGL_lose_context | null
+type GLProgram = WebGLProgram
+interface GLShader extends WebGLShader {}
 type GLTexture = WebGLTexture | null
+type GLUniformLocation = WebGLUniformLocation | null
