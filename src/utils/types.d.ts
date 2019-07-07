dcf20a1e6ab40679bdc1a6a768a7893a13004a21
Stephen Niedzielski <stephen@niedzielski.com>
2019-07-06 20:21:47 -0600

    [dev] move types to utils and drop Omit

--- src/utils/types.d.ts
+++ src/utils/types.d.ts
@@ -1,4 +1,3 @@
-type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>
 type ValueOf<T> = T[keyof T]
 type Mutable<T> = {-readonly [K in keyof T]: T[K]}
 type Never<T, V> = T extends V ? never : T
