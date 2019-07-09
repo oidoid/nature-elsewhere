b6697437c9ff9aa681f9b7555a83bcaa49f2e3d5
Stephen Niedzielski <stephen@niedzielski.com>
2019-07-08 18:44:07 -0600

    [fix] [dev] fix and simplify Random

--- src/math/random.ts
+++ src/math/random.ts
@@ -1,20 +1,21 @@
 // http://www.firstpr.com.au/dsp/rand31/
 // https://jsperf.com/park-miller-vs-math-random
 export class Random {
-  /** @arg _seed An integer [-2^31 + 3, 2^31 - 2] or [-0x7fff_fffd, 0x7fff_fffe]. */
-  constructor(private _seed: number) {
-    this._seed = _seed > 0 ? _seed % 0x7fff_ffff : _seed + 0x7fff_fffe
+  /** @arg seed An integer. */
+  constructor(private seed: number) {
+    this.seed = seed % 0x7fff_ffff // [-0x7fff_fffe, 0x7fff_fffe]
+    if (this.seed <= 0) this.seed += 0x7fff_ffff // [1, 0x7fff_ffff]
   }
 
-  /** @return [min, max) */
-  float(min: number = 0, max: number = 1): number {
-    // [1, 2^31 - 1] or [0x1, 0x7fff_ffff]
-    this._seed = (this._seed * 16807) % 0x7fff_ffff
-    return min + (max - min) * ((this._seed - 1) / 0x7fff_fffe)
+  /** @return [0, 1) */
+  float(): number {
+    return this.int() / 0x7fff_fffe
   }
 
-  /** @return An integer [min, max). */
-  int(min = 0, max = Number.MAX_SAFE_INTEGER): number {
-    return Math.floor(this.float(min, max))
+  /** @return An integer [0, 2^31 - 3]. */
+  int(): number {
+    // [1, 2^31 - 2] or [0x1, 0x7fff_fffe]
+    this.seed = (this.seed * 16_807) % 0x7fff_ffff
+    return this.seed - 1
   }
 }
