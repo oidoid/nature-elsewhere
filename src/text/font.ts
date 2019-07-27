29494b33c5ab99dcde30c14f0aa2c673e7272e91
Stephen Niedzielski <stephen@niedzielski.com>
2019-07-27 15:28:05 -0600

    [dev] move Font props out of records

--- src/text/font.ts
+++ src/text/font.ts
@@ -2,41 +2,53 @@ import {ObjectUtil} from '../utils/object-util'
 
 export interface Font {
   /** Height of lines, not including descenders, in pixels. */
-  letterHeight: number
+  readonly letterHeight: number
 
   /** Distance between lines in pixels. */
-  leading: number
+  readonly leading: number
 
   /** letterHeight + leading. */
-  lineHeight: number
+  readonly lineHeight: number
 
   /** Distance between letters in pixels. */
-  kerning: Readonly<Record<string, number>> &
-    Readonly<{
-      default: number
-      whitespace: number
-      end: number
-    }>
+  readonly kerning: Readonly<Record<string, number>>
+  readonly defaultKerning: number
+  readonly whitespaceKerning: number
+  readonly endOfLineKerning: number
 
   /** Vertical top offset in pixels. */
-  letterOffset: Readonly<Record<string, number>> & Readonly<{default: number}>
+  readonly letterOffset: Readonly<Record<string, number>>
+  readonly defaultLetterOffset: number
 
   /** Character width in pixels. */
-  letterWidth: Readonly<Record<string, number>> & Readonly<{default: number}>
+  readonly letterWidth: Readonly<Record<string, number>>
+  readonly defaultLetterWidth: number
 }
 
 export namespace Font {
-  export function kerning({kerning}: Font, lhs: string, rhs?: string): number {
-    if (rhs === undefined) return kerning.end
-    if (/\s/.test(lhs + rhs)) return kerning.whitespace
-    return ObjectUtil.defaultIfAbsent(kerning, lhs + rhs, kerning.default)
+  export function kerning(font: Font, lhs: string, rhs?: string): number {
+    if (rhs === undefined) return font.endOfLineKerning
+    if (/\s/.test(lhs + rhs)) return font.whitespaceKerning
+    return ObjectUtil.defaultIfAbsent(
+      font.kerning,
+      lhs + rhs,
+      font.defaultKerning
+    )
   }
 
-  export function letterOffset({letterOffset}: Font, char: string): number {
-    return ObjectUtil.defaultIfAbsent(letterOffset, char, letterOffset.default)
+  export function letterOffset(font: Font, char: string): number {
+    return ObjectUtil.defaultIfAbsent(
+      font.letterOffset,
+      char,
+      font.defaultLetterOffset
+    )
   }
 
-  export function letterWidth({letterWidth}: Font, char: string): number {
-    return ObjectUtil.defaultIfAbsent(letterWidth, char, letterWidth.default)
+  export function letterWidth(font: Font, char: string): number {
+    return ObjectUtil.defaultIfAbsent(
+      font.letterWidth,
+      char,
+      font.defaultLetterWidth
+    )
   }
 }
