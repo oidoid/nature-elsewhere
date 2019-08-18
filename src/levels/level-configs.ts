9805610250f826d731363b44807c71bbe039e1d0
Stephen Niedzielski <stephen@niedzielski.com>
2019-08-17 21:44:04 -0600

    [dev] add entities level

--- src/levels/level-configs.ts
+++ src/levels/level-configs.ts
@@ -1,9 +1,10 @@
 import {Level} from './level'
 import * as editor from '../assets/levels/editor.json'
+import * as entities from '../assets/levels/entities.json'
 import * as fields from '../assets/levels/fields.json'
 import * as shader from '../assets/levels/shader.json'
 import * as title from '../assets/levels/title.json'
 
 export const LevelConfigs: Readonly<
   Record<string, Level.Config>
-> = Object.freeze({editor, fields, shader, title})
+> = Object.freeze({editor, entities, fields, shader, title})
