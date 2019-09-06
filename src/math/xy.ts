27d37c4612da3acd9434639914df5bc91ebea5f4
Stephen Niedzielski <stephen@niedzielski.com>
2019-09-06 17:25:36 -0600

    [dev] refactor XY

--- src/math/xy.ts
+++ src/math/xy.ts
@@ -1,27 +1,29 @@
-export interface XY {
-  readonly x: number
-  readonly y: number
-}
+export type XY = Readonly<{x: number; y: number}>
+type t = XY
 
 export namespace XY {
-  export function add(lhs: XY, rhs: XY | number): XY {
-    if (typeof rhs === 'number') return {x: lhs.x + rhs, y: lhs.y + rhs}
-    return {x: lhs.x + rhs.x, y: lhs.y + rhs.y}
-  }
+  export const add = (lhs: t, rhs: t): t => ({
+    x: lhs.x + rhs.x,
+    y: lhs.y + rhs.y
+  })
 
-  export function sub(lhs: XY, rhs: XY): XY {
-    return {x: lhs.x - rhs.x, y: lhs.y - rhs.y}
-  }
+  export const sub = (lhs: t, rhs: t): t => ({
+    x: lhs.x - rhs.x,
+    y: lhs.y - rhs.y
+  })
 
-  export function min(lhs: XY, rhs: XY): XY {
-    return {x: Math.min(lhs.x, rhs.x), y: Math.min(lhs.y, rhs.y)}
-  }
+  export const min = (lhs: t, rhs: t): t => ({
+    x: Math.min(lhs.x, rhs.x),
+    y: Math.min(lhs.y, rhs.y)
+  })
 
-  export function max(lhs: XY, rhs: XY): XY {
-    return {x: Math.max(lhs.x, rhs.x), y: Math.max(lhs.y, rhs.y)}
-  }
+  export const max = (lhs: t, rhs: t): t => ({
+    x: Math.max(lhs.x, rhs.x),
+    y: Math.max(lhs.y, rhs.y)
+  })
 
-  export function trunc({x, y}: XY): XY {
-    return {x: Math.trunc(x), y: Math.trunc(y)}
-  }
+  export const trunc = ({x, y}: t): t => ({
+    x: Math.trunc(x),
+    y: Math.trunc(y)
+  })
 }
