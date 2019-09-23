e7a833a39dff71c07176ee66891640eb8d5a1f1e
Stephen Niedzielski <stephen@niedzielski.com>
2019-09-23 13:39:10 -0600

    [dev] [JSON Schema] [experimental] replace custom parsers with JSON Schema and Ajv

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,15 @@
 import {AssetLoader} from './loaders/AssetLoader'
 import {Game} from './Game'
 import {Settings} from './settings/Settings'
+import {SchemaValidator} from './schemas/SchemaValidator'
+import {Definition} from './schemas/Schema'
+import {JSON} from './utils/jsonUtil/JSONUtil'
+import * as foo from './foo.json'
 ;(async () => {
   const canvas = document.querySelector('canvas')
   if (!canvas) throw new Error('Canvas missing.')
+
+  console.log(SchemaValidator.validate(<JSON>(<any>foo), Definition.AnyEntity))
 
   const assets = await AssetLoader.load()
   const game = Game.make(window, canvas, assets, Settings.defaults)
