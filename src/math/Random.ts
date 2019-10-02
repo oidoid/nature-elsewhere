3451cd0b81ff72fc12116bc4f7328cdbb17f16b0
Stephen Niedzielski <stephen@niedzielski.com>
2019-10-02 14:24:11 -0600

    [Random] rename private

--- src/math/Random.ts
+++ src/math/Random.ts
@@ -1,10 +1,10 @@
 // http://www.firstpr.com.au/dsp/rand31/
 // https://jsperf.com/park-miller-vs-math-random
 export class Random {
-  /** @arg seed An integer. */
-  constructor(private seed: number) {
-    this.seed = seed % 0x7fff_ffff // [-0x7fff_fffe, 0x7fff_fffe]
-    if (this.seed <= 0) this.seed += 0x7fff_ffff // [1, 0x7fff_ffff]
+  /** @arg _seed An integer. */
+  constructor(private _seed: number) {
+    this._seed = _seed % 0x7fff_ffff // [-0x7fff_fffe, 0x7fff_fffe]
+    if (this._seed <= 0) this._seed += 0x7fff_ffff // [1, 0x7fff_ffff]
   }
 
   /** @return [0, 1) */
@@ -15,7 +15,7 @@ export class Random {
   /** @return An integer [0, 2^31 - 3]. */
   int(): number {
     // [1, 2^31 - 2] or [0x1, 0x7fff_fffe]
-    this.seed = (this.seed * 16_807) % 0x7fff_ffff
-    return this.seed - 1
+    this._seed = (this._seed * 16_807) % 0x7fff_ffff
+    return this._seed - 1
   }
 }
