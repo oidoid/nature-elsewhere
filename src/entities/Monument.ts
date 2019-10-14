d4d2eabb074d3c2aac053edaa81d84df39ddb7ba
Stephen Niedzielski <stephen@niedzielski.com>
2019-10-14 10:09:46 -0600

    [Monument] add small variant

--- src/entities/Monument.ts
+++ src/entities/Monument.ts
@@ -20,18 +20,16 @@ export class Monument extends Entity<Monument.Variant, Monument.State> {
   ) {
     super({
       ...defaults,
-      collisionBodies: defaults.collisionBodies.map(Rect.copy),
+      collisionBodies: defaults.collisionBodies[
+        (props && props.variant) || defaults.variant
+      ].map(Rect.copy),
       map: {
         [Entity.BaseState.HIDDEN]: new ImageRect(),
         [Monument.State.VISIBLE]: new ImageRect({
-          images: [
-            new Image(atlas, {id: AtlasID.MONUMENT}),
-            new Image(atlas, {
-              id: AtlasID.MONUMENT_SHADOW,
-              position: new XY(0, 1),
-              layer: Layer.SHADOW
-            })
-          ]
+          images: variantImages(
+            atlas,
+            (props && props.variant) || defaults.variant
+          )
         })
       },
       ...props
@@ -45,7 +43,8 @@ export class Monument extends Entity<Monument.Variant, Monument.State> {
 
 export namespace Monument {
   export enum Variant {
-    NONE = 'none'
+    SMALL = 'small',
+    MEDIUM = 'medium'
   }
 
   export enum State {
@@ -55,9 +54,34 @@ export namespace Monument {
 
 const defaults = ObjectUtil.freeze({
   type: EntityType.MONUMENT,
-  variant: Monument.Variant.NONE,
+  variant: Monument.Variant.SMALL,
   state: Monument.State.VISIBLE,
   collisionPredicate: CollisionPredicate.BODIES,
-  collisionBodies: [Rect.make(6, 35, 25, 11), Rect.make(5, 46, 28, 6)],
+  collisionBodies: {
+    [Monument.Variant.SMALL]: [
+      Rect.make(7, 28, 23, 3),
+      Rect.make(24, 23, 15, 6)
+    ],
+    [Monument.Variant.MEDIUM]: [
+      Rect.make(6, 35, 25, 11),
+      Rect.make(5, 46, 28, 6)
+    ]
+  },
   collisionType: CollisionType.TYPE_SCENERY | CollisionType.OBSTACLE
 })
+
+function variantImages(atlas: Atlas, variant: Monument.Variant): Image[] {
+  const small = variant === Monument.Variant.SMALL
+  return [
+    new Image(atlas, {
+      id: small ? AtlasID.MONUMENT_SMALL : AtlasID.MONUMENT_MEDIUM
+    }),
+    new Image(atlas, {
+      id: small
+        ? AtlasID.MONUMENT_SMALL_SHADOW
+        : AtlasID.MONUMENT_MEDIUM_SHADOW,
+      position: new XY(0, 1),
+      layer: Layer.SHADOW
+    })
+  ]
+}
