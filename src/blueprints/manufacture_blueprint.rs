fd8a6563976a9b4e251e3e54d201dc02093ccf99
Stephen Niedzielski <stephen@niedzielski.com>
2020-04-05 22:38:20 -0600

    [dev] [blueprints] move Sprite manufacturing

--- src/blueprints/manufacture_blueprint.rs
+++ src/blueprints/manufacture_blueprint.rs
@@ -1,6 +1,8 @@
-use super::{AlignToBlueprint, WHBlueprint, XYBlueprint};
+use super::{AlignToBlueprint, SpriteBlueprint, WHBlueprint, XYBlueprint};
+use crate::atlas::{Animator, Atlas};
 use crate::components::AlignTo;
-use crate::math::{WH, XY};
+use crate::math::{R16, WH, XY};
+use crate::sprites::{Sprite, SpriteComposition, SpriteLayer};
 
 /// Unfortunately, due to the deep patching of components, type mapping with
 /// `deserialize_with` is not possible. Thus, component blueprints are patches
@@ -17,6 +19,97 @@ impl ManufactureBlueprint<AlignTo> for Option<AlignToBlueprint> {
     } else {
       None
     }
+  }
+}
+
+impl SpriteBlueprint {
+  pub fn manufacture(&self, atlas: &Atlas) -> Sprite {
+    //another name is props
+    let id = self.id;
+    let constituent_id = self.constituent_id.unwrap_or(id);
+    let composition = self.composition.unwrap_or(SpriteComposition::Source);
+    let scale = self
+      .scale
+      .clone()
+      .map_or(XY::new(self.sx.unwrap_or(1), self.sy.unwrap_or(1)), |scale| {
+        XY::new(scale.x.unwrap_or(1), scale.y.unwrap_or(1))
+      });
+    // valid ate scale is nonzero or enforce it with templatized XY<nonzero thingy>
+    let position =
+      self.position.clone().map_or(
+        XY::new(self.x.unwrap_or(0), self.y.unwrap_or(0)),
+        |position| XY::new(position.x.unwrap_or(0), position.y.unwrap_or(0)),
+      );
+    let animation = &atlas.animations[&id];
+    // do i need to validate area too?
+    let area = self.area.clone().map_or(
+      WH::new(
+        self.w.unwrap_or(animation.wh.w),
+        self.h.unwrap_or(animation.wh.h),
+      ),
+      |area| WH::new(area.w.unwrap_or(0), area.h.unwrap_or(0)),
+    ); //use atlas, review ts
+    let bounds = self.bounds.clone().map_or(
+      R16::cast_wh(position.x, position.y, area.w, area.h),
+      |bounds| {
+        R16::cast_wh(
+          bounds.x.unwrap_or(0),
+          bounds.y.unwrap_or(0),
+          bounds.w.unwrap_or(0),
+          bounds.h.unwrap_or(0),
+        )
+      },
+    );
+
+    let layer = self.layer.unwrap_or(SpriteLayer::Default);
+    let animator = self.animator.clone().map_or(
+      Animator::new(
+        animation,
+        self.period.unwrap_or(0),
+        self.exposure.unwrap_or(0.),
+      ),
+      |animator| {
+        Animator::new(
+          animation,
+          animator.period.unwrap_or(0),
+          animator.exposure.unwrap_or(0.),
+        )
+      },
+    );
+    let wrap = self
+      .wrap
+      .clone()
+      .map_or(XY::new(self.wx.unwrap_or(0), self.wy.unwrap_or(0)), |wrap| {
+        XY::new(wrap.x.unwrap_or(0), wrap.y.unwrap_or(0))
+      });
+    let wrap_velocity = self.wrap_velocity.clone().map_or(
+      XY::new(self.wvx.unwrap_or(0), self.wvy.unwrap_or(0)),
+      |wrap_velocity| {
+        XY::new(wrap_velocity.x.unwrap_or(0), wrap_velocity.y.unwrap_or(0))
+      },
+    );
+    let source = R16::cast_wh(
+      animation.cels[0].xy.x,
+      animation.cels[0].xy.y,
+      animation.wh.w,
+      animation.wh.h,
+    );
+    let constituent = R16::cast_wh(
+      atlas.animations[&constituent_id].cels[0].xy.x,
+      atlas.animations[&constituent_id].cels[0].xy.y,
+      atlas.animations[&constituent_id].wh.w,
+      atlas.animations[&constituent_id].wh.h,
+    );
+    // how does this work with animations? offsets are applied to everything.
+    Sprite::new(
+      source,
+      constituent,
+      composition,
+      bounds,
+      scale,
+      wrap,
+      wrap_velocity,
+    )
   }
 }
 
